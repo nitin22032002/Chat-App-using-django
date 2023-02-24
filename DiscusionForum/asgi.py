2911f6daffb5ee63a8574ae7d6a0f305f8ae6fab
Nitin Gupta <gnitin2203@gmail.com>
2023-02-24 17:27:00 +0530

    fix-updates

--- DiscusionForum/asgi.py
+++ DiscusionForum/asgi.py
@@ -5,6 +5,7 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from chatRoom.routing import wm_patterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DiscusionForum.settings')
+django.setup()
 application = ProtocolTypeRouter({
     "websocket":AuthMiddlewareStack(URLRouter(wm_patterns)),
     "http":get_asgi_application()
